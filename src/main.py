f782d0b11dbd308738fe8af98659f7cb24b47db3
Josue Andre da Silva Barros <Josue.Barross@alphait.us>
2023-06-18 23:42:47 -0300

    feat: added Player FOV & 3D Rendering algorithm for Walls

--- src/main.py
+++ src/main.py
@@ -5,14 +5,16 @@ from settings import *
 from map_renderer import MapRenderer
 from player import Player
 from bsp import BSP
+from seg_handler import SegHandler
+from view_renderer import ViewRenderer
 
 class DoomEngine:
   def __init__(self, wad_path='./resources/wad/DOOM1.WAD'):
     self.wad_path = wad_path
-    self.screen = pg.display.set_mode(WIN_RES)
+    self.screen = pg.display.set_mode(WIN_RES, pg.SCALED)
     self.clock = pg.time.Clock()
     self.running = True
-    self.dt = 1/ 60
+    self.dt = 1 / 60
     self.on_init()
 
   def on_init(self):
@@ -20,30 +22,38 @@ class DoomEngine:
     self.map_renderer = MapRenderer(self)
     self.player = Player(self)
     self.bsp = BSP(self)
+    self.seg_handler = SegHandler(self)
+    self.view_renderer = ViewRenderer(self)
 
   def update(self):
     self.player.update()
+    self.seg_handler.update()
     self.bsp.update()
     self.dt = self.clock.tick()
-    pg.display.set_caption(f'{self.clock.get_fps()}')
+    pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
 
   def draw(self):
-    self.screen.fill('black')
-    self.map_renderer.draw()
-    pg.display.flip()
+    pg.display.flip() # Debug Draw
+    # self.screen.fill('black')
+    # self.map_renderer.draw()
+    # pg.display.flip()
 
   def check_events(self):
     for e in pg.event.get():
       if e.type == pg.QUIT:
         self.running = False
+        pg.quit()
+        sys.exit()
 
   def run(self):
-    while self.running:
+    fps = 0
+    n = 15000
+    for i in range(n):
       self.check_events()
       self.update()
       self.draw()
-    pg.quit()
-    sys.exit()
+      fps += self.clock.get_fps()
+    print(fps / n)
 
 if __name__ == '__main__':
   doom = DoomEngine()
--- src/player.py
+++ src/player.py
@@ -8,6 +8,39 @@ class Player:
     self.thing = engine.wad_data.things[0]
     self.pos = self.thing.pos
     self.angle = self.thing.angle
+    self.height = PLAYER_HEIGHT
+    self.DIAGONAL_MOVE_CORRECTION = 1 / math.sqrt(2)
 
   def update(self):
-    pass
+    self.get_height()
+    self.control()
+
+  def get_height(self):
+    self.height = self.engine.bsp.get_sub_sector_height() + PLAYER_HEIGHT
+
+  def control(self):
+    speed = PLAYER_SPEED * self.engine.dt
+    rot_speed = PLAYER_ROT_SPEED * self.engine.dt
+
+    keys = pg.key.get_pressed()
+
+    if keys[pg.K_LEFT]:
+      self.angle += rot_speed
+    if keys[pg.K_RIGHT]:
+      self.angle -= rot_speed
+
+    inc = vec2(0)
+    if keys[pg.K_a]:
+      inc += vec2(0, speed)
+    if keys[pg.K_d]:
+      inc += vec2(0, -speed)
+    if keys[pg.K_w]:
+      inc += vec2(speed, 0)
+    if keys[pg.K_s]:
+      inc += vec2(-speed, 0)
+
+    if inc.x and inc.y:
+      inc *= self.DIAGONAL_MOVE_CORRECTION
+
+    inc.rotate_ip(self.angle)
+    self.pos += inc
